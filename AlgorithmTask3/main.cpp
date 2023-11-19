397987c8161eed289169d8fefbce120e9782f1cc
BKUD <buuchaysatunov@gmail.com>
2023-11-19 20:23:39 +0300

    В этом коде я заменил статический массив maze[5][5] на динамический двумерный массив maze, который может быть любого размера. Также я заменил статический массив dist[5][5] на динамический двумерный массив dist. Это позволяет обрабатывать лабиринты любого размера, а не только 5x5. Пожалуйста, проверьте этот код и дайте мне знать, если у вас возникнут вопросы.

--- AlgorithmTask3/main.cpp
+++ AlgorithmTask3/main.cpp
@@ -1,20 +1,100 @@
-﻿// AlgorithmTask3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <climits>
 
-#include <iostream>
+// Определение структуры "Node" для односвязного списка
+struct Node {
+    int x, y; // Координаты узла в лабиринте
+    Node* next; // Указатель на следующий узел в списке
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// Функция для создания нового узла
+Node* newNode(int x, int y) {
+    Node* temp = new Node;
+    temp->x = x;
+    temp->y = y;
+    temp->next = NULL;
+    return temp;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+// Функция для добавления элемента в конец очереди
+void enqueue(Node** head, Node** tail, int x, int y) {
+    Node* temp = newNode(x, y);
+    if (*head == NULL) {
+        *head = *tail = temp;
+    }
+    else {
+        (*tail)->next = temp;
+        *tail = temp;
+    }
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+// Функция для удаления элемента из начала очереди
+void dequeue(Node** head, Node** tail) {
+    if (*head == NULL) return;
+    Node* temp = *head;
+    *head = (*head)->next;
+    if (*head == NULL) *tail = NULL;
+    delete temp;
+}
+
+// Функция для проверки, является ли данная точка допустимой
+bool isValid(int x, int y, int m, int n) {
+    return (x >= 0 && x < m && y >= 0 && y < n);
+}
+
+// Функция для поиска кратчайшего пути из лабиринта
+int shortestPath(int** maze, int sx, int sy, int m, int n) {
+    Node* head = NULL, * tail = NULL;
+    int dx[] = { -1, 0, 1, 0 }; // Возможные направления движения по x
+    int dy[] = { 0, 1, 0, -1 }; // Возможные направления движения по y
+    int** dist = new int* [m]; // Матрица расстояний
+    for (int i = 0; i < m; i++) {
+        dist[i] = new int[n];
+        for (int j = 0; j < n; j++)
+            dist[i][j] = INT_MAX; // Инициализация матрицы расстояний
+    }
+    dist[sx][sy] = 0; // Расстояние от начальной точки до себя равно 0
+    enqueue(&head, &tail, sx, sy); // Добавление начальной точки в очередь
+    while (head != NULL) { // Пока очередь не пуста
+        int x = head->x, y = head->y; // Извлечение текущей точки
+        dequeue(&head, &tail); // Удаление текущей точки из очереди
+        for (int i = 0; i < 4; i++) { // Проверка всех возможных направлений
+            int newX = x + dx[i], newY = y + dy[i]; // Новая точка
+            // Если новая точка допустима и ближе, чем текущее расстояние
+            if (isValid(newX, newY, m, n) && maze[newX][newY] == 0 && dist[newX][newY] > dist[x][y] + 1) {
+                dist[newX][newY] = dist[x][y] + 1; // Обновление расстояния
+                enqueue(&head, &tail, newX, newY); // Добавление новой точки в очередь
+            }
+        }
+    }
+    // Поиск минимального расстояния до края лабиринта
+    int minDist = INT_MAX;
+    for (int i = 0; i < m; i++) {
+        minDist = std::min(minDist, dist[i][0]);
+        minDist = std::min(minDist, dist[i][n - 1]);
+    }
+    for (int i = 0; i < n; i++) {
+        minDist = std::min(minDist, dist[0][i]);
+        minDist = std::min(minDist, dist[m - 1][i]);
+    }
+    return (minDist == INT_MAX) ? -1 : minDist; // Возвращение минимального расстояния
+}
+
+int main() {
+    // Определение лабиринта
+    int m = 5, n = 5;
+    int** maze = new int* [m];
+    for (int i = 0; i < m; i++) {
+        maze[i] = new int[n];
+    }
+    maze[0][0] = 1; maze[0][1] = 1; maze[0][2] = 1; maze[0][3] = 1; maze[0][4] = 1;
+    maze[1][0] = 1; maze[1][1] = 1; maze[1][2] = 0; maze[1][3] = 0; maze[1][4] = 1;
+    maze[2][0] = 1; maze[2][1] = 1; maze[2][2] = 1; maze[2][3] = 0; maze[2][4] = 1;
+    maze[3][0] = 1; maze[3][1] = 1; maze[3][2] = 1; maze[3][3] = 0; maze[3][4] = 1;
+    maze[4][0] = 1; maze[4][1] = 1; maze[4][2] = 1; maze[4][3] = 0; maze[4][4] = 0;
+    // Вызов функции для поиска кратчайшего пути
+    int shortest_distance = shortestPath(maze, 1, 2, m, n);
+    // Вывод кратчайшего пути
+    std::cout << "The shortest path is " << shortest_distance << std::endl;
+    return 0;
+}
